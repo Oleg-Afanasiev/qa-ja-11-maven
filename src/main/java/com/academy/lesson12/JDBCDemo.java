275f076d50ebe0c5da89a8629d98d525f1eb7476
af_oleg <oleg.kh81@gmail.com>
2021-06-15 20:15:38 +0300

    lesson 12 -> JDBC select impl

--- src/main/java/com/academy/lesson12/JDBCDemo.java
+++ src/main/java/com/academy/lesson12/JDBCDemo.java
@@ -10,22 +10,22 @@ public class JDBCDemo {
         String url = "jdbc:mysql://localhost:3306/phone-book?user=root&password=root";
         String sqlSelect = "select * from subscriber";
 
-        try {
-            Connection connection = DriverManager.getConnection(url);
+        try(Connection connection = DriverManager.getConnection(url)) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelect);
 
-            resultSet.next(); // переводим курсор на первую полученную строку
-            String firstName = resultSet.getString("first_name");
-            String lastName = resultSet.getString("last_name");
-            int age = resultSet.getInt("age");
-            Gender gender = Gender.parse(resultSet.getString("gender"));
+            while (resultSet.next()) { // переводим курсор на след строку. Возвращает null, если данных нет
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                int age = resultSet.getInt("age");
+                Gender gender = Gender.parse(resultSet.getString("gender"));
 
-            System.out.println(firstName);
-            System.out.println(lastName);
-            System.out.println(age);
-            System.out.println(gender);
+                System.out.printf("%s %s %d %s %n",
+                        firstName, lastName, age, gender);
+            }
 
+            statement.close();
+            resultSet.close();
         } catch (SQLException e) {
             System.out.println("Connection error. Details: " + e.getMessage());
         }
