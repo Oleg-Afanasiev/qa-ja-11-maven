43cab4093e7f17f2542dacb8193e63c951322757
af_oleg <oleg.kh81@gmail.com>
2021-06-15 20:23:36 +0300

    lesson 12 -> JDBC insert impl

--- src/main/java/com/academy/lesson12/JDBCDemo.java
+++ src/main/java/com/academy/lesson12/JDBCDemo.java
@@ -9,8 +9,21 @@ public class JDBCDemo {
         // select
         String url = "jdbc:mysql://localhost:3306/phone-book?user=root&password=root";
         String sqlSelect = "select * from subscriber";
+        String sqlInsert = "insert into subscriber " +
+                "(first_name, last_name, age, gender) " +
+                "values(?, ?, ?, ?)";
 
         try(Connection connection = DriverManager.getConnection(url)) {
+            // insert
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert);
+            preparedStatement.setString(1, "Вася"); // values(?, ?, ?, ?) -> values('Вася', ?, ?, ?)
+            preparedStatement.setString(2, "Кириленко");  // values('Вася', ?, ?, ?) -> values('Вася', 'Кириленко', ?, ?)
+            preparedStatement.setInt(3, 18);
+            preparedStatement.setString(4, "m");
+            preparedStatement.execute();
+            preparedStatement.close();
+
+            // select
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sqlSelect);
 
